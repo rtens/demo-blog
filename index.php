6035cca8dd7f1ca8f77662957571c109272ebf89
Nikolas Martens <Nikolas.M@rtens.org>
2015-03-04 14:27:27 +0100

    Getting rid of PHP

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
-$articles = glob('articles/*.html');
-include('index.html');
+require_once 'vendor/autoload.php';
+
+$viewModel = [
+    'article' => assembleArticles()
+];
+
+$renderer = new \watoki\tempan\Renderer(file_get_contents('index.html'));
+echo $renderer->render($viewModel);
+
+function assembleArticles()
+{
+    $articles = [];
+    foreach (glob('articles/*.html') as $article) {
+        list($date, $title) = explode('__', basename($article));
+
+        $articles[] = [
+            'link' => array('href' => $article),
+            'date' => date('Y, F dS', strtotime($date)),
+            'title' => str_replace(['_', '.html'], ' ', $title)
+        ];
+    }
+    return $articles;
+}
