88824984e45008e4342c7132a95d93ed6c685d6b
Nikolas Martens <Nikolas.M@rtens.org>
2015-03-05 11:29:29 +0100

    Article Not Found

--- ArticleResource.php
+++ ArticleResource.php
@@ -1,14 +1,22 @@
 <?php
 
+use watoki\curir\delivery\WebResponse;
+use watoki\curir\error\HttpError;
+
 class ArticleResource extends \watoki\curir\Resource {
 
     public function doGet($article) {
+        $file = "articles/$article.html";
+        if (!file_exists($file)) {
+            throw new HttpError(WebResponse::STATUS_NOT_FOUND, "The article [$article] does not exist.");
+        }
+
         list($date, $title) = explode('__', $article);
 
         return [
             'title' => ucfirst(str_replace('_', ' ', $title)),
             'date' => date('Y, F dS', strtotime($date)),
-            'content' => file_get_contents("articles/$article.html"),
+            'content' => file_get_contents($file),
             'commentOn' => ['value' => $article],
             'comment' => $this->assembleComments($article)
         ];
